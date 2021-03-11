6b5a92eb74f5c400f7a82cc26f0309092b25219a
Prashant Baid <prashantbaid80@gmail.com>
2021-03-11 16:09:34 +0530

    Get Sports News

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from newsapi import NewsApiClient
+from decouple import config
 
+api = NewsApiClient(api_key=config('APIKEY'))
+top_headlines = api.get_top_headlines(
+    country='in', category='sports', language='en')['articles']
+
+
+def get_sports_news():
+    for i in range(3):
+        print(top_headlines[i]['title'])
+
+
+get_sports_news()
